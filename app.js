078a3ec1cd3bb6883c9a16810e542543f5c6109a
mudassir089 <61446880+mudassir089@users.noreply.github.com>
2020-09-24 20:44:41 +0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function offbulb() {
-  var bulb = document.getElementById("bulb");
-  bulb.src = "off.png";
-}
-
-function onbulb() {
-  var bulb = document.getElementById("bulb");
-  bulb.src = "on.png";
-}
